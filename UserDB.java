e801b07c1c08287db1f32e99cd4ce6e059440890
chernhaw <38425578+chernhaw@users.noreply.github.com>
2021-07-11 11:00:49 +0800

    Update UserDB.java

--- UserDB.java
+++ UserDB.java
@@ -22,16 +22,22 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class UserDB {
+	
+	
+	
 
-	public static int insert (User User)  {
+	public static int insert (String email, String firstName, String lastName)  {
 		
 		
-		
-		
-		//get a connection
 		String dbURL = "jdbc:postgresql://localhost:5432/mydb";
 		String username = "postgres";
 		String password = "postgres";
+		
+		//get a connection
+		System.out.println(" preparing to populate user.user with "
+				+ "\n email:"+email+
+				"\n firstname:" +firstName+
+				"\n lastname:" +lastName);
 		
 		PreparedStatement ps = null;
 		Connection connection = null;
@@ -47,7 +53,7 @@ public class UserDB {
 		}
 		
 		int min = 1;
-		int max = 100;
+		int max = 10000;
 		
 	      int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
 	      System.out.println("Random value "+random_int);
@@ -65,9 +71,9 @@ public class UserDB {
 			try {
 				ps = connection.prepareStatement(query);
 				ps.setInt(1,  random_int);
-				ps.setString(2,  User.getEmail());
-				ps.setString(3,  User.getFirstname());
-				ps.setString(4,  User.getLastname());
+				ps.setString(2,  email);
+				ps.setString(3,  firstName);
+				ps.setString(4,  lastName);
 				ps.setDate(5, sqlDate);
 				
 				
@@ -84,8 +90,224 @@ public class UserDB {
 					System.out.println(e);
 				}
 				
+			}		
+			
+	}
+	
+	public static int updatebyEmail(User user) {
+		String dbURL = "jdbc:postgresql://localhost:5432/mydb";
+		String username = "postgres";
+		String password = "postgres";
+		
+		//get a connection
+		
+		PreparedStatement ps = null;
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(dbURL,username, password);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String query = "update public.user set "
+						+"firstname = ?, "
+						+"lastname = ? "
+						+"where email = ?";
+		
+		try {
+			ps = connection.prepareStatement(query);
+		
+			ps.setString(1,  user.getFirstname());
+			ps.setString(2,  user.getLastname());
+			ps.setString(3,  user.getEmail());
+			System.out.println(user.getEmail());
+			System.out.println(user.getLastname());
+			return ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+			return 1;
+		} finally {
+			try {
+				ps.close();
+				connection.close();
+			} catch (Exception e) {
+				System.out.println(e);
+				return 1;
 			}
 			
+		}
+		
+			
+	}
+	
+	public static User getUserByEmail(String email) {
+		String dbURL ="jdbc:postgresql://localhost:5432/mydb";
+		String username ="postgres";
+		String password ="postgres";
+		ResultSet rs = null;
+		
+		PreparedStatement ps = null;
+		Connection connection = null;
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(dbURL, username, password);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String query = "select * from public.user where email= ?";
+		
+		try {
+			ps=connection.prepareStatement(query);
+			ps.setString(1,  email);
+			rs=ps.executeQuery();
+			
+			if (rs.next()) {
+				
+				User user = new User();;
+				user.setUserid(rs.getInt("userid"));
+				user.setEmail(rs.getString("email"));
+				user.setLastname(rs.getString("lastname"));
+				user.setFirstname(rs.getString("firstname"));
+				user.setGrant_date(rs.getDate("grant_date"));
+				return user;
+				
+			}
+		} catch (SQLException e1) {
+			e1.getStackTrace();
+			
+		}
+		return null;
+	}
+	
+	public static User getUserById(int id) {
+		String dbURL = "jdbc:postgresql://localhost:5432/mydb";
+		String username = "postgres";
+		String password = "postgres";
+		ResultSet rs = null;
+		
+		//get a connection
+		
+		PreparedStatement ps = null;
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(dbURL,username, password);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String query = "select * from public.user  "
+						+ "  where userid = ?";
+		
+		try {
+			ps = connection.prepareStatement(query);
+		
+			ps.setInt(1,  id);
+			rs=ps.executeQuery();
+			
+			
+			
+//			userid     | integer                     |           | not null | 
+//			 email      | character varying(50)       |           |          | 
+//			 firstname  | character varying(50)       |           |          | 
+//			 lastname   | character varying(50)       |           |          | 
+//			 grant_date | timestamp without time zone |
+			if(rs.next()) {
+				User user = new User();;
+				user.setUserid(rs.getInt("userid"));
+				user.setEmail(rs.getString("email"));
+				user.setLastname(rs.getString("lastname"));
+				user.setFirstname(rs.getString("firstname"));
+				user.setGrant_date(rs.getDate("grant_date"));
+				return user;
+			}
+			
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+			return null;
+		} finally {
+			try {
+				ps.close();
+				connection.close();
+			} catch (Exception e) {
+				System.out.println(e);
+				return null;
+			}
+			
+		}
+		return null;
+		
+			
+	}
+	
+	public static int updatebyId(User user, int id) {
+		String dbURL = "jdbc:postgresql://localhost:5432/mydb";
+		String username = "postgres";
+		String password = "postgres";
+		
+		//get a connection
+		
+		PreparedStatement ps = null;
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection(dbURL,username, password);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String query = "update public.user set "
+						+"firstname = ?, "
+						+"lastname = ?, "
+						+"email = ? "
+						+"where userid=?";
+		
+		try {
+			ps = connection.prepareStatement(query);
+		
+			ps.setString(1,  user.getFirstname());
+			ps.setString(2,  user.getLastname());
+			ps.setString(3,  user.getEmail());
+			ps.setInt(4,  user.getUserid());
+			System.out.println(user.getEmail());
+			System.out.println(user.getLastname());
+			return ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+			return 1;
+		} finally {
+			try {
+				ps.close();
+				connection.close();
+			} catch (Exception e) {
+				System.out.println(e);
+				return 1;
+			}
+			
+		}
+		
 			
 	}
 }
